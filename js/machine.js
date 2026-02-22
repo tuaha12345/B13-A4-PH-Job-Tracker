94a2bcd818f8a3fc5c2dcf62b5e8dc72ed41bb5e
Syeed md tuaha <syeedmdtuaha@gmail.com>
2026-02-22 17:49:13 +0600

    index.js and machine.js created

---
+++ js/machine.js
@@ -0,0 +1,16 @@
+function getValue(id)
+{
+    return document.getElementById(id).value;
+}
+function setValue(id, value)
+{
+    document.getElementById(id).value = value;
+}
+function getInnerText(id)
+{
+    return document.getElementById(id).innerText;
+}
+function setInnerText(id, value)
+{
+    document.getElementById(id).innerText = value;
+}
