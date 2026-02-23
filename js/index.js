4eec058cfbc467e4da72222e70fd0b7f4796aed3
Syeed md tuaha <syeedmdtuaha@gmail.com>
2026-02-23 11:29:36 +0600

    reject & interview functionality added

--- js/index.js
+++ js/index.js
@@ -29,19 +29,25 @@ for(x of jobs)
                 <li class="text-[14px] text-[#64748B]">•</li>
                 <li class="text-[14px] text-[#64748B]">${x.type}</li>
                 <li class="text-[14px] text-[#64748B]">•</li>
-                <li class="text-[14px] text-[#64748B]">$${x.salary}</li>
+                <li class="text-[14px] text-[#64748B]">${x.salary}</li>
             </ul>
         </div>
         <div class="card-button">
             <button class="btn btn-soft btn-info text-gray-700 p-3 mt-5">NOT APPLIED</button>
+            <button class="btn btn-success text-amber-50 p-3 mt-5 hidden">INTERVIEW</button>
+            <button class="btn btn-error text-amber-50 p-3 mt-5 hidden">REJECTED</button>
         </div>
         <div class="card-job-description">
             <p class="text-[14px] mt-5">${x.description}</p>
         </div>
         <div class="card-footer-button">
-            <button class="btn btn-outline btn-success p-3 mt-5">INTERVIEW</button>
-             <button class="btn btn-outline btn-error p-3 mt-5 ml-2">REJECTED</button>
+            <button class="btn btn-outline btn-success p-3 mt-5" onclick="interviewJob(${x.id})">INTERVIEW</button>
+             <button class="btn btn-outline btn-error p-3 mt-5 ml-2" onclick="rejectJob(${x.id})">REJECTED</button>
         </div>
     </div>`;
     document.getElementById("jobContainer").innerHTML+=cards;
 }
+
+let interviewCards=[];
+let rejectedCards=[];
+
